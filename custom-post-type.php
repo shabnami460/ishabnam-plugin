54586d9bd5f46bb50675a3c51c26f1c4793c1a2c
shabnami460 <shabnami@sheridancollege.ca>
2016-08-20 12:28:46 -0400

    new-post-page

--- custom-post-type.php
+++ custom-post-type.php
@@ -22,7 +22,7 @@
 		$singular = 'Portfolio';
 		
 		$labels = array(
-			'name'			=> 'Portfolio',
+			'name'			=> $singular,
 			'add_name'		=> 'Add New',
 			'add_new_item'	=> 'Add new experience to your ' . $singular,
 			'edit'			=> 'Edit',
@@ -54,14 +54,57 @@
 				'title',
 				'editor',
 				'author',
-				'category',
 				'thumbnail',
 			)
-		); 	
+		); 
 
 		register_post_type( 'portfolio', $args);
 	}
 	add_action( 'init', 'ishabnam_register_post_type' );
+	
+				
+	function ishabnam_register_taxonomy(){
+	
+		$singular = 'Categories';
+	
+		$labels = array(
+			'name'              => $singular,
+			'search_items'      => 'Search ' . $singular,
+			'all_items'         => 'All' . $singular,
+			'parent_item'       => 'Parent' . $singular,
+			'parent_item_colon' => 'Parent ' . $singular,
+			'edit_item'         => 'Edit' . $singular,
+			'update_item'       => 'Update' . $singular,
+			'add_new_item'      => 'Add New' . $singular,
+			'new_item_name'     => 'New ' . $singular . ' Name',
+			'menu_name'         => $singular,
+		);	
+		
 
+				
+	$args = array(
+		'public'				=> true,
+		'hierarchical'      	=> false,
+		'labels'            	=> $labels,
+		'show_ui'           	=> true,
+		'show_admin_column'		=> true,
+		'query_var'         	=> true,
+		'exclude_from_search'	=> true,
+		'rewrite'  => array(
+			'slug' => 'category',
+		'support' => array(
+			'title',
+			'author',
+			'category',
+			)
+		)	
 
+	);
+
+		register_taxonomy( 'categories', 'portfolio', $args);
+	
+	}
+	add_action( 'init' , 'ishabnam_register_taxonomy');
+
+	
 ?>
